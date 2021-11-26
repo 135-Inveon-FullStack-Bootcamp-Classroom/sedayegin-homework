4bc2072c475705ec41a4c899e711549976b8719a
sedayegin <86283908+sedayeginn@users.noreply.github.com>
2021-11-26 21:06:21 +0300

    hatalar duzenlendi

--- 6-InveonMoviesWebApi/6-InveonMoviesWebApi/Concrete/Movie.cs
+++ 6-InveonMoviesWebApi/6-InveonMoviesWebApi/Concrete/Movie.cs
@@ -8,11 +8,10 @@ namespace _6_InveonMoviesWebApi.Concrete
     public class Movie :IBaseEntity
     {
         public string Name { get; set; }
-        public string Genre { get; set; }
         public string Details { get; set; }
-        public IList<Actor> Actors { get; set; }
-        public IList<Genre> Genres { get; set; }
         public Producer Producer { get; set; }
         public int ProducerId { get; set; }
+        public IList<Actor> Actors { get; set; }
+        public IList<Genre> Genres { get; set; }
     }
 }
