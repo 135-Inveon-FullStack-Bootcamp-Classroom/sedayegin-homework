8eea0994f4376623813efd889a699caff295cf90
sedayegin <86283908+sedayeginn@users.noreply.github.com>
2021-11-29 23:17:45 +0300

    unitOfWork

--- 7-InveonFootballManager/FootballManagerApi/UnitOfWork/IUnitOfWork.cs
+++ 7-InveonFootballManager/FootballManagerApi/UnitOfWork/IUnitOfWork.cs
@@ -7,6 +7,12 @@ namespace FootballManagerApi.UnitOfWork
     {
         public ITeamService TeamService { get; set; }
         public IFootballerService FootballerService { get; set; }
+        public ICoachService CoachService { get; set; }
+        public IManagementPositionService ManagementPositionService { get; set; }
+        public INationalTeamService NationalTeamService { get; set; }
+        public IPositionService PositionService { get; set; }
+        public ITacticService TacticService { get; set; }
+        public IManagerService ManagerService { get; set; }
         Task SaveChangesAsync();
     }
 }
--- 7-InveonFootballManager/FootballManagerApi/UnitOfWork/UnitOfWork.cs
+++ 7-InveonFootballManager/FootballManagerApi/UnitOfWork/UnitOfWork.cs
@@ -8,15 +8,27 @@ namespace FootballManagerApi.UnitOfWork
     {
         private readonly ApplicationDbContext _dbContext;
 
-        public UnitOfWork(ITeamService teamService, IFootballerService footballerService, ApplicationDbContext dbContext)
+        public UnitOfWork(ITeamService teamService, IFootballerService footballerService, ICoachService coachService, IManagementPositionService managementPositionService, INationalTeamService nationalTeamService, IPositionService positionService, ITacticService tacticService, IManagerService managerService, ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
             this.FootballerService = footballerService;
             this.TeamService = teamService;
+            this.ManagerService = managerService;
+            this.NationalTeamService = nationalTeamService;
+            this.ManagementPositionService = managementPositionService;
+            this.PositionService = positionService;
+            this.ManagerService = managerService;
+            this.TacticService = tacticService;
         }
 
         public ITeamService TeamService { get; set; }
         public IFootballerService FootballerService { get; set; }
+        public ICoachService CoachService { get; set; }
+        public IManagementPositionService ManagementPositionService { get; set; }
+        public INationalTeamService NationalTeamService { get; set; }
+        public IPositionService PositionService { get; set; }
+        public ITacticService TacticService { get; set; }
+        public IManagerService ManagerService { get; set; }
 
         public async Task SaveChangesAsync()
         {
